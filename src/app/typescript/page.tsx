4c46d9fbb58d740507d7d456e63c7b2d514832a4
Muhammad Zubair <mzzubair3@gmail.com>
2024-10-21 21:44:33 +0500

    Third Commit

--- src/app/typescript/page.tsx
+++ src/app/typescript/page.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const page = () => {
+  interface IUser {
+    name: string;
+    email: string;
+    age: number;
+  }
+  interface IId extends IUser {
+    id: number;
+  }
+  const user: IId = {
+    name: "Zubair",
+    email: "zubair@test.com",
+    age: 44,
+    id: 12,
+  };
+  console.log(user);
   return (
     <div>
       <div>Hello World</div>
